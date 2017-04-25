c7aadd957559c4d84e3926844daee2142f749e92
Eli Nemzer <elinemzer7@gmail.com>
2017-04-25 11:17:27 -0400

    starting to get it

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const express = require( 'express' );
 const app = express(); // creates an instance of an express application
-// const bodyParser = require('body-parser');
-// app.use(function (req, res, next) {
-//     console.log('Request: ' + req);
-//     console.log('Route? ' + req.params);
-//     next();
-// })
-const volleyball = require('volleyball')
+const bodyParser = require('body-parser');
+const nunjucks = require('nunjucks');
+const volleyball = require('volleyball');
+app.set('view engine', 'html'); // have res.render work with html files
+app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
+nunjucks.configure('views', {noCache: true}); // point nunjucks to the proper directory for templates
 
-app.use(volleyball)
+
+const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+
+app.use('/', function(req, res, next){
+  console.log(req.url);
+  next();
+});
+
+app.use(volleyball);
 
 app.use('/special/', function (req, res, next) {
     console.log('You reached the special area');
@@ -16,16 +23,15 @@ app.use('/special/', function (req, res, next) {
 });
 
 app.get('/', function (req, res, next){
-  res.send('Welcome!');
-  next();
+  res.render( 'index', {title: 'Hall of Fame', people: people} );
+//  res.send('Welcome!');
+//  next();
 });
 
 app.get('/news', function (req, res, next){
   res.send('Welcome News!');
-  next();
+//  next();
 });
-
-
 
 
 var PORT = 3000;
@@ -33,3 +39,18 @@ var PORT = 3000;
   app.listen(PORT, function(){
     console.log('server listening');
   });
+
+//   var locals =  {
+//     title: 'An Example',
+//     people: [
+//       {name: 'Gandalf'},
+//       {name: 'Frodo'},
+//       {name: 'Hermione'}
+//   ]
+// };
+
+// nunjucks.configure('views', {noCache: true});
+// nunjucks.render('index.html', locals, function (err, output){
+//   if (err) throw err;
+//   console.log(output);
+// })
